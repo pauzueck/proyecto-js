957e5464f6139f757a3a0717f71cf7cf90efe316
paulina zueck <pauzueck@gmail.com>
2023-11-07 18:10:44 -0600

    objetos

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,5 @@
+//Login Marketplace
+
 const USERNAME= prompt("Username: ").toLowerCase();
 const PASSWORD= prompt("Password: ").toLowerCase();
 
@@ -5,4 +7,25 @@ if(USERNAME === "plantlover" &&(PASSWORD === "plantlife"|| PASSWORD === "1234"))
     console.log("Welcome to PlantLife Marketplace!");
 } else {
     console.log("Try again!");
+}
+
+
+// Declarar los objetos
+
+const plant1 = { 
+    plantName: "Monstera", 
+    price: 28.00, 
+    currency: "USD" 
+}
+
+const plant2 = { 
+    plantName: "Peperomia", 
+    price: 30.00, 
+    currency: "USD" 
+}
+
+const plant3 = { 
+    plantName: "Rubber Plant", 
+    price: 25.00, 
+    currency: "USD" 
 }
