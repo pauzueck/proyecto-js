ea95b292f5e2db1c39d1e0c8ead11957893bc7ca
paulina zueck <pauzueck@gmail.com>
2023-12-29 18:08:28 -0600

    fetch and styles

--- js/index.js
+++ js/index.js
@@ -19,10 +19,14 @@ class Plant {
     const JADE = new Plant(3, "Jade Plant", 18.00, "USD", "./assets/jadeplant.jpg");
     const PEACE = new Plant(3, "Peace Lilly", 22.00, "USD", "./assets/peacelilly.jpg");
     const SNAKE = new Plant(3, "Snake Plant", 20.00, "USD", "./assets/snakeplant.jpg");
+    const PEACOCK = new Plant(3, "Peacock Plant", 18.00, "USD", "./assets/peacockplant.jpg");
+    const FICUS = new Plant(3, "Ficus", 13.00, "USD", "./assets/ficus.jpg");
+    const POTHOS = new Plant(3, "Pothos", 16.00, "USD", "./assets/pothos.jpg");
 
 
 
-    const plantsCollection = [MONSTERA, PEPEROMIA, RUBBER, FIDDLE, JADE, PEACE, SNAKE];    
+
+    const plantsCollection = [MONSTERA, PEPEROMIA, RUBBER, FIDDLE, JADE, PEACE, SNAKE, PEACOCK, FICUS, POTHOS];    
 
 //Array carrito
 
@@ -46,7 +50,7 @@ class Plant {
                 <img src="${plant.img}" class="card-img-tom imgPlant" />
                 <div class="card-body">
                     <h2>${plant.plantName}</h2>
-                    <p>${plant.price} ${plant.currency}</p>
+                    <p>$ ${plant.price} ${plant.currency}</p>
                     <button onclick="addToCart(${plant.id}, '${plant.plantName}', ${plant.price}, '${plant.currency}')" class="styleButton" id="boton${plant.id}">Add to Cart</button>
                     </div>
                 </div>            
@@ -145,4 +149,57 @@ class Plant {
                 icon: "warning",
             });
     }
+    
+    const loginButton = document.querySelector('.loginButton');
+    const modal = document.getElementById('loginModal');
+    const loginForm = document.getElementById('loginForm');
+
+
+    loginButton.addEventListener('click', () => {
+        modal.style.display = 'block';
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username === "plantlover" && (password === "plantlife" || PASSWORD === "1234")) {
+        console.log("Welcome to PlantLife Marketplace!");
+        modal.style.display = 'none';
+    } else {
+        alert('Please try again.');
+        document.getElementById('username').value = '';
+        document.getElementById('password').value = '';
+    }
+});
+
+const apiWeather = 'https://api.openweathermap.org/data/2.5/weather?lat=19.4326&lon=-99.133209&appid=8aae4d886afb9f947d510793e90c54a0';
+
+function updateTemperature(temperature) {
+const temperatureElement = document.getElementById('temperature');
+    temperatureElement.textContent = `Today's Temperature: ${temperature}°C`; 
+}
+
+    fetch(apiWeather)
+    .then(response => {
+        if (!response.ok) {
+        throw new Error('Error!');
+        }
+        return response.json();
+    })
+    .then(data => {
+        const temperature = data.main.temp; 
+        const temperatureInCelsius = Math.round(temperature - 273.15); 
+        updateTemperature(temperatureInCelsius);
+    })
+    .catch(error => {
+        console.error('Error fetching weather data:', error);
+    });
     
